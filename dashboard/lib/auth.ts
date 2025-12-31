4d7dd499312ac4175ef2029f59a062c012adc07c
ismat-samadov <ismetsemedov@gmail.com>
2025-12-31 20:08:03 +0400

    init

--- dashboard/lib/auth.ts
+++ dashboard/lib/auth.ts
@@ -12,8 +12,20 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   pages: {
     signIn: "/login",
   },
+  cookies: {
+    sessionToken: {
+      name: process.env.NODE_ENV === 'production'
+        ? '__Secure-next-auth.session-token'
+        : 'next-auth.session-token',
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: process.env.NODE_ENV === 'production',
+      },
+    },
+  },
   trustHost: true,
-  useSecureCookies: process.env.NODE_ENV === 'production',
   providers: [
     CredentialsProvider({
       name: "credentials",
